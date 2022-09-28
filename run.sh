fc79addcded959d8f9c64ea6f782e1fa8b27e7d7
bonjour <clonekim@gmail.com>
2022-09-28 11:12:31 +0900

    추가 'run.sh'

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+  echo "Usage $0 [start|stop]"
+  exit 1
+fi
+
+args="$1"
+
+case $args in
+  start)
+	  nohup pshttp >console.log 2>&1 &
+	  echo $! > pid
+    ;;
+
+  stop)
+    PID="$PWD/pid"
+    echo "kill $(<"$PID")"
+    kill $(<"$PID")
+    rm $PID
+    ;;
+
+  *)
+    echo "unknown argument"
+    exit 1
+    ;;
+esac
