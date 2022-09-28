e8a9ce538f28cd17d5959bec4e7a1b7d9eeaeca6
daehee.kim <p.daehee@kalmate.net>
2022-09-28 17:30:10 +0900

    update

---
+++ server/pipeline.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bytes"
+	"os"
+	"os/exec"
+)
+
+func Output(commands ...[]string) ([]byte, error) {
+	cmds := make([]*exec.Cmd, len(commands))
+	var err error
+
+	for i, c := range commands {
+		cmds[i] = exec.Command(c[0], c[1:]...)
+		if i > 0 {
+			if cmds[i].Stdin, err = cmds[i-1].StdoutPipe(); err != nil {
+				return nil, err
+			}
+		}
+		cmds[i].Stderr = os.Stderr
+	}
+	var out bytes.Buffer
+	cmds[len(cmds)-1].Stdout = &out
+	for _, c := range cmds {
+		if err = c.Start(); err != nil {
+			return nil, err
+		}
+	}
+	for _, c := range cmds {
+		if err = c.Wait(); err != nil {
+			return nil, err
+		}
+	}
+	return out.Bytes(), nil
+}
+
+// CombinedOutput runs the commands and returns its combined standard output and standard error.
+func CombinedOutput(commands ...[]string) ([]byte, error) {
+	cmds := make([]*exec.Cmd, len(commands))
+	var err error
+
+	var out bytes.Buffer
+	for i, c := range commands {
+		cmds[i] = exec.Command(c[0], c[1:]...)
+		if i > 0 {
+			if cmds[i].Stdin, err = cmds[i-1].StdoutPipe(); err != nil {
+				return nil, err
+			}
+		}
+		cmds[i].Stderr = &out
+	}
+	cmds[len(cmds)-1].Stdout = &out
+	for _, c := range cmds {
+		if err = c.Start(); err != nil {
+			return out.Bytes(), err
+		}
+	}
+	for _, c := range cmds {
+		if err = c.Wait(); err != nil {
+			return out.Bytes(), err
+		}
+	}
+	return out.Bytes(), nil
+}
